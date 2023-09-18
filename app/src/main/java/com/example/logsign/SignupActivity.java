5fcd836e951a3898998524ae10861fb0b9a6f451
Fjdwe19 <Fjdwe22@gmail.com>
2023-09-19 01:42:40 +0700

    LogSIgn

---
+++ app/src/main/java/com/example/logsign/SignupActivity.java
@@ -0,0 +1,60 @@
+package com.example.logsign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.os.Bundle;
+
+import com.example.logsign.databinding.ActivitySignupBinding;
+
+public class SignupActivity extends AppCompatActivity {
+    ActivitySignupBinding binding;
+    DatabaseHelper databaseHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        databaseHelper = new DatabaseHelper(this);
+        binding.signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.signupEmail.getText().toString();
+                String password = binding.signupPassword.getText().toString();
+                String confirmPassword = binding.signupConfirm.getText().toString();
+                if(email.equals("")||password.equals("")||confirmPassword.equals(""))
+                    Toast.makeText(SignupActivity.this, "Monggo Diisi Semua ya", Toast.LENGTH_SHORT).show();
+                else{
+                    if(password.equals(confirmPassword)){
+                        Boolean checkUserEmail = databaseHelper.checkEmail(email);
+                        if(checkUserEmail == false){
+                            Boolean insert = databaseHelper.insertData(email, password);
+                            if(insert == true){
+                                Toast.makeText(SignupActivity.this, "Sign Up Berhasil Derr", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(SignupActivity.this, "Ngapunten Sign Up Gagal Derr", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(SignupActivity.this, "SIlahkan Login Derr", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(SignupActivity.this, "Password salah Derr", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        binding.loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
